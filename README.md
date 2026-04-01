7f9d006ca17181c5de5861dddd647e9d2ef96da1
Wesly Pardillo <200398940+Wesly19-max@users.noreply.github.com>
2026-04-01 18:13:22 +0800

    Add function to play single game

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Requirements
 V1:
 
 [X]a way to get computer choice
-[]a way to get human choice
-[]a way to store their scores
-[]a way to play a single round
+[X]a way to get human choice
+[X]a way to store their scores
+[X]a way to play a single round
 []a way to play multiple rounds
--- app.js
+++ app.js
@@ -18,34 +18,49 @@ function getHumanChoice() {
 }
 
 
-//[]a way to store their scores
-let humanScore =0;
-let computerScore =0;
+//a way to play multiple rounds
+function playGame() {
+
+    //a way to store their scores
+  let humanScore =0;
+  let computerScore =0;
 
 
-//[]a way to play a single round
-function playRound(humanChoice,computerChoice) {
-  if ((humanChoice=="rock" && computerChoice=="scissors")||
-      (humanChoice=="paper" && computerChoice=="rock")||
-      (humanChoice=="scissors" && computerChoice=="paper")) 
-  {
-    humanScore ++;
-    console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+  //a way to play a single round
+  function playRound(humanChoice,computerChoice) {
+    if ((humanChoice=="rock" && computerChoice=="scissors")||
+        (humanChoice=="paper" && computerChoice=="rock")||
+        (humanChoice=="scissors" && computerChoice=="paper")) 
+    {
+      humanScore ++;
+      console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+    }
+    else if (humanChoice == computerChoice)
+    {
+      console.log("It's a tie!")
+    }else {
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+    }
+
   }
-  else if (humanChoice == computerChoice)
-  {
-    console.log("It's a tie!")
+
+    
+  //calls playRound function 5 times
+  for (let i =0;i<5;i++) {
+    playRound(getHumanChoice(),getComputerChoice());
+  }
+  
+  //declare the winner
+  if (humanScore > computerScore) {
+    console.log("You won!")
+  }else if (humanScore<computerScore) {
+    console.log("You lost!")
   }else {
-    computerScore++;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+    console.log("It's a tie!")
   }
-
+  
 }
 
-
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection,computerSelection);
-//[]a way to play multiple rounds
+//play a single game (5 rounds)
+playGame();
