c28dc6c6483576861bbd687f66afa43243e90a35
Wesly Pardillo <200398940+Wesly19-max@users.noreply.github.com>
2026-04-03 20:43:14 +0800

    Refactor playRound function

--- app.js
+++ app.js
@@ -22,10 +22,7 @@ function playGame() {
   const yourScoreBoard = document.querySelector(".your-score");
   const computerScoreBoard = document.querySelector(".computer-score");
 
-
-
-
-
+  const roundMessage = document.querySelector(".round-message");
   //a way to play a single round
   function playRound(humanChoice,computerChoice) {
     if ((humanChoice=="rock" && computerChoice=="scissors")||
@@ -33,21 +30,22 @@ function playGame() {
         (humanChoice=="scissors" && computerChoice=="paper")) 
     {
       humanScore ++;
-      console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+      
+      roundMessage.textContent= `You won! ${humanChoice} beats ${computerChoice}`
     }
     else if (humanChoice == computerChoice)
     {
-      console.log("It's a tie!")
+      roundMessage.textContent = "It's a tie!"
     }else {
       computerScore++;
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+      roundMessage.textContent = `You lose! ${computerChoice} beats ${humanChoice}`
     }
 
   }
 
-      //display score board
-  yourScoreBoard.textContent = `Your score: ${humanScore}`;
-  computerScoreBoard.textContent = `Computer Score : ${computerScore}`
+        //display score board
+    yourScoreBoard.textContent = `Your score: ${humanScore}`;
+    computerScoreBoard.textContent = `Computer Score : ${computerScore}`
     //for each button
     const buttons = document.querySelectorAll("button");
     
